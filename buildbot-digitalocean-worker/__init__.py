480c10fc6dc021279de5b008d65a58964ca391ab
John Ramsden <johnramsden@users.noreply.github.com>
2017-09-21 16:05:57 -0700

    Implement Latent class and access regions

--- buildbot-digitalocean-worker/__init__.py
+++ buildbot-digitalocean-worker/__init__.py
@@ -1,21 +1,78 @@
 import digitalocean
 from digitalocean import SSHKey
 
-# def create_droplet():
-    # actions = droplet.get_actions()
-    # for action in actions:
-    #     action.load()
-    #     # Once it shows complete, droplet is up and running
-    #     print(action.status)
+from buildbot import config
+from buildbot.interfaces import LatentWorkerFailedToSubstantiate
+from buildbot.worker import AbstractLatentWorker
+from buildbot.worker_transition import reportDeprecatedWorkerNameUsage
 
-def main():
-    my_api_token=''
 
-    user_ssh_key = open('/home/john/.ssh/id_ssh_rsa.pub').read()
-    key = SSHKey(token=my_api_token,
-                 name='chin_id',
-                 public_key=user_ssh_key)
-    key.create()
+class DigitalOceanLatentWorker(AbstractLatentWorker):
+
+    def __init__(self, name, password, image, api_token, region=None,
+                 size_slug=None, backups=False, user_data=None, **kwargs):
+
+        AbstractLatentWorker.__init__(self, name, password, **kwargs)
+
+        self.api_token = api_token
+
+        if region is None:
+            usable_region = self._get_available_region()
+            if usable_region is not None:
+                self.region = usable_region
+                print("Set region to:", self.region)
+            else:
+                print("No regions available")
+        else:
+            self.set_region(region)
+
+        self.name = name
+        self.password = password
+        self.image = image
+
+
+    def set_region(self, region):
+        do_region = digitalocean.Region(token=self.api_token)
+
+        region_data = region.get_data("/v2/regions")
+
+    def start_instance(self):
+        print("Starting instance.")
+
+    def stop_instance(self):
+        print("Stopping instance.")
+
+    def _get_available_region(self):
+        available_region = None
+        do_region = digitalocean.Region(token=self.api_token)
+
+        region_data = do_region.get_data("/v2/regions")
+
+        for r in region_data.get('regions'):
+            if r is not None:
+                """Default to region nyc1 if unset"""
+                if r['slug'] is 'nyc1':
+                    if r['available'] is True:
+                        available_region = r['slug']
+                    else:
+                        available_region = None
+
+                """If nyc1 unavailable, keep looking for available"""
+                if available_region is None:
+                    if r['available'] is True:
+                        available_region = r['slug']
+
+        return available_region
+
+
+
+
+def create_droplet():
+    # user_ssh_key = open('/home/john/.ssh/id_ssh_rsa.pub').read()
+    # key = SSHKey(token=my_api_token,
+    #              name='chin_id',
+    #              public_key=user_ssh_key)
+    # key.create()
     manager = digitalocean.Manager(token=my_api_token)
     keys = manager.get_all_sshkeys()
 
@@ -39,12 +96,17 @@ def main():
                                       - sudo -H --user buildbot /home/buildbot/venv/bin/pip install --upgrade pip
                                       - sudo -H --user buildbot /home/buildbot/venv/bin/pip install --upgrade setuptools-trial
                                       - sudo -H --user buildbot /home/buildbot/venv/bin/pip install --upgrade buildbot-worker
-                                      - sudo -H --user buildbot /home/buildbot/venv/bin/buildbot-worker create-worker worker localhost example-worker pass
                                       - mkdir -p /opt/builbotdata
                                       - git clone https://github.com/johnramsden/bez /opt/builbotdata/bez
                                    """)
     droplet.create()
 
+    #- sudo -H --user buildbot /home/buildbot/venv/bin/buildbot-worker create-worker worker localhost example-worker pass
+
+def main():
+    my_api_token=''
+
+    do_latent_worker = DigitalOceanLatentWorker("Bob", "pass", "image", my_api_token)
 
 if __name__ == "__main__":
     # execute only if run as a script
