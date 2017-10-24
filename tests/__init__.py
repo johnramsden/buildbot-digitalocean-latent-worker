32914e2028fffa52f6440464402c0d69e13f0dab
John Ramsden <johnramsden@users.noreply.github.com>
2017-10-24 10:08:54 -0700

    Start adding test

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,10 +2,25 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+from buildbot.plugins import worker, util, schedulers, changes, steps
+
+import password
+import workers
+
 import DigitalOceanLatentWorker
 
 def create_worker():
     print("Creating test worker")
+
+    bez_git_repo = 'https://{}{}@github.com/johnramsden/bez'.format(
+        password.github_token, ":x-oauth-basic")
+
+    dolw = worker.DigitalOceanLatentWorker(
+        "bez-worker", password.bez_worker_pass,
+        "ubuntu-16-04-x64", password.digitalocean_api_key,
+        ssh_keys=['chin_id'], user_data=workers.do_worker_user_data['ubuntu']['user_data'])
+
+    dolw.start_instance(True)
 
 def main():
     create_worker()
